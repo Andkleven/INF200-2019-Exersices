ba6d25f8652d96095dfafbbd4eb19abedf9865f6
And <andermat@nmbu.no>
2019-10-04 09:55:51 +0100

    Added: test_one_element

--- src/anders_mathiesen_ex/ex03_project/test_median.py
+++ src/anders_mathiesen_ex/ex03_project/test_median.py
@@ -7,4 +7,24 @@ __email__ = 'andermat@nmbu.no'
 import pytest
 
 
+def median(data):
+    """
+    Returns median of data.
+
+    :param data: An iterable of containing numbers
+    :return: Median of data
+    """
+
+    sdata = sorted(data)
+    n = len(sdata)
+    if n == 0:
+        raise ValueError("Can find median of a empty list")
+    return (sdata[n // 2] if n % 2 == 1
+            else 0.5 * (sdata[n // 2 - 1] + sdata[n // 2]))
+
+
+def test_one_element():
+    test_list = [1]
+    median_list = median(test_list)
+    assert median_list == 1
 
