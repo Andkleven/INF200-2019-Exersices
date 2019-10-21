4037bccc9d664b8b20a5242d7086e7824e7c73ba
And <andermat@nmbu.no>
2019-10-21 16:49:38 +0100

    single_game

--- src/pa01/snakes_and_ladders.py
+++ src/pa01/snakes_and_ladders.py
@@ -1,20 +1,35 @@
 def single_game(num_players):
+    """
+        Returns duration of single game.
+
+        Arguments
+        ---------
+        num_players : int
+            Number of players in the game
+
+        Returns
+        -------
+        num_moves : int
+            Number of moves the winning player needed to reach the goal
+        """
     import random
+    ladders = {1: 39, 8: 2, 36: 16, 43: 19, 49: 30, 65: 17, 68: 14}
+    snakes = {24: -19, 33: -30, 42: -12, 56: -19, 64: -37, 74: -62, 87: -17}
+    throws_all_players = []
+    for _ in range(num_players):
+        throws = 0
+        place = 0
+        while place < 90:
+            place += random.randint(1, 6)
+            if place in ladders:
+                place += ladders[place]
+            elif place in snakes:
+                place += snakes[place]
+            throws += 1
+        throws_all_players.append(throws)
 
-    random.randint(1, 6)
-    """
-    Returns duration of single game.
+    return min(throws_all_players)
 
-    Arguments
-    ---------
-    num_players : int
-        Number of players in the game
-
-    Returns
-    -------
-    num_moves : int
-        Number of moves the winning player needed to reach the goal
-    """
 
 def multiple_games(num_games, num_players):
     """
@@ -32,6 +47,7 @@ def multiple_games(num_games, num_players):
     num_moves : list
         List with the numbedr of moves needed in each game.
     """
+
 
 def multi_game_experiment(num_games, num_players, seed):
     """
