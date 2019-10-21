dc25a1644587ffa2f846e7ea6ccafbf3cba7bead
And <andermat@nmbu.no>
2019-10-21 16:55:22 +0100

    merge

--- src/pa01/snakes_and_ladders.py
+++ src/pa01/snakes_and_ladders.py
@@ -75,9 +75,6 @@ def multi_game_experiment(num_games, num_players, seed):
     -------
     num_moves : list
 
-        List with the numbedr of moves needed in each game.
-    """
-
         List with the number of moves needed in each game.
     """
 
