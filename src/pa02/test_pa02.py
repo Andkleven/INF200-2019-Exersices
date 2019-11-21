362b94ce23c3cb7337e9a10d0d258715873cd1f9
Kristian Gunder Kramås <kristiankramas@outlook.com>
2019-11-22 00:46:08 +0100

    Tests

--- src/pa02/test_pa02.py
+++ src/pa02/test_pa02.py
@@ -7,19 +7,18 @@ Extended set of tests for PA02.
 __author__ = 'Anders Mathiesen, Kristian Kramås'
 __email__ = 'andermat@nmbu.no, kristiakr@nmbu.no'
 
-
+import random
 import src.pa02.chutes_simulation as cs
-# import pytest
 
 
 class TestBoard:
     """
-    Additional tests for Board class.
+    Additional test(s) for Board class.
     """
     pass
 
     def test_chutes_go_down(self):
-        """se """
+        """chutes go down"""
         b = cs.Board()
         for position in range(b.goal):
             old_position = position
@@ -32,8 +31,7 @@ class TestBoard:
                 assert(new_position < old_position)
 
     def test_ladders_go_up(self):
-        """se """
-        b = cs.Board()
+        """ladders go up"""
         b = cs.Board()
         for position in range(b.goal):
             old_position = position
@@ -46,46 +44,135 @@ class TestBoard:
                 assert (new_position > old_position)
 
 
-
 class TestPlayer:
     """
-    Additional tests for Player class.
+    Additional test(s) for Player class.
     """
     def test_position_positive(self):
         """position positive after move"""
         b = cs.Board()
         p = cs.Player(b)
-        for _ in range(20):
+        for _ in range(b.goal):
             p.move()
             assert(p.position > 0)
 
     def test_move_changes_position(self):
-        """description"""
+        """position changes on move"""
         b = cs.Board()
         p = cs.Player(b)
-        for _ in range(20):
+        for _ in range(b.goal):
             old_position = p.position
             p.move()
             assert (p.position != old_position)
 
+    def test_move_positive(self):
+        """move is positive without ladders and chutes"""
+        b = cs.Board()
+        b.ladders = []
+        b.chutes = []
+        p = cs.Player(b)
+        for _ in range(b.goal):
+            old_position = p.position
+            p.move()
+            assert(p.position > old_position)
+
 
 class TestResilientPlayer:
     """
-    Additional tests for ResilientPlayer class.
+    Additional test(s) for ResilientPlayer class.
     """
-    pass
+
+    def test_resilient_player_is_better(self):
+        """checks if resilient players are faster on average"""
+        b = cs.Board()
+        b.ladders = []
+        p = cs.Player(b)
+        pr = cs.ResilientPlayer(b, 100)
+
+        p_wins = 0
+        pr_wins = 0
+
+        random.seed(12345)
+
+        for _ in range(10000):
+            p_count = 0
+            pr_count = 0
+            p.position = 0
+            pr.position = 0
+
+            while p.position < b.goal:
+                p.move()
+                p_count += 1
+
+            while pr.position < b.goal:
+                pr.move()
+                pr_count += 1
+
+            if pr_count < p_count:
+                pr_wins += 1
+            elif p_count < pr_count:
+                p_wins += 1
+
+        print(pr_wins, p_wins)
+
+        if p_wins > 0:
+            # This factor can be adjusted using statistics,
+            # but we won't in this case.
+            factor = 2.0
+            assert(pr_wins/p_wins > factor)
+        elif pr_wins == 0:
+            assert False
 
 
 class TestLazyPlayer:
     """
-    Additional tests for LazyPlayer class.
+    Additional test(s) for LazyPlayer class.
     """
-    pass
+
+    def test_lazy_player_is_worse(self):
+        """checks if lazy players are slower on average"""
+        b = cs.Board()
+        b.chutes = []
+        p = cs.Player(b)
+        pl = cs.LazyPlayer(b, 100)
+
+        p_wins = 0
+        pl_wins = 0
+
+        random.seed(12345)
+
+        for _ in range(10000):
+            p_count = 0
+            pl_count = 0
+            p.position = 0
+            pl.position = 0
+
+            while p.position < b.goal:
+                p.move()
+                p_count += 1
+
+            while pl.position < b.goal:
+                pl.move()
+                pl_count += 1
+
+            if pl_count < p_count:
+                pl_wins += 1
+            elif p_count < pl_count:
+                p_wins += 1
+
+        print(pl_wins, p_wins)
+
+        if pl_wins > 0 and p_wins > 0:
+            # This factor can be adjusted using statistics,
+            # but we won't in this case.
+            factor = 1.3
+            assert (p_wins/pl_wins > factor)
+        else:
+            assert False
 
 
 class TestSimulation:
     """
-    Additional tests for Simulation class.
+    Additional test(s) for Simulation class.
     """
     pass
-
