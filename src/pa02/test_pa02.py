61f17d866904f807dee2f342b4b9382b2df59e94
Kristian Gunder Kramås <kristiankramas@outlook.com>
2019-11-21 16:13:49 +0100

    Test

--- src/pa02/test_pa02.py
+++ src/pa02/test_pa02.py
@@ -8,7 +8,49 @@ __author__ = 'Anders Mathiesen, Kristian Kramås'
 __email__ = 'andermat@nmbu.no, kristiakr@nmbu.no'
 
 
+import src.pa02.chutes_simulation as cs
+import pytest
+
+
 class TestBoard:
     """
     Additional tests for Board class.
     """
+    pass
+
+
+class TestPlayer:
+    """
+    Additional tests for Player class.
+    """
+    def test_position_positive(self):
+        """position positive after move"""
+        b = cs.Board()
+        p = cs.Player()
+        p.move()
+        assert(p.position > 1)
+
+    def function(self):
+        """description"""
+        pass
+
+class TestResilientPlayer:
+    """
+    Additional tests for ResilientPlayer class.
+    """
+    pass
+
+
+class TestLazyPlayer:
+    """
+    Additional tests for LazyPlayer class.
+    """
+    pass
+
+
+class TestSimulation:
+    """
+    Additional tests for Simulation class.
+    """
+    pass
+
